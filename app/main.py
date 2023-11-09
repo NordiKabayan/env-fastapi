f34e3874ca99bfd91f067b600c78b908e757272b
Herman Menor <herman.menor@gmail.com>
2023-11-09 15:03:52 +0200

    main.py: experiment on variables

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import redis
 
 app = FastAPI()
+r = redis.Redis(host="redis", port=6379)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "NordiKabayan.tech"}
+    return {"Hello": "NordiKabayan.gg"}
+
+@app.get("/count")
+def read_root():
+    r.incr("index")
+    return { "Hit Count": r.get("index") }
